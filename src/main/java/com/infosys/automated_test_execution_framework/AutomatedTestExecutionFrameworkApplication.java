7288c14c26681d91dc13f67b243e5aeb3c918acf
Tanaybobbili <tanaybobbili.129@gmail.com>
2026-01-13 16:34:30 +0530

    first commit

---
+++ src/main/java/com/infosys/automated_test_execution_framework/AutomatedTestExecutionFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.automated_test_execution_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTestExecutionFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedTestExecutionFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosys/automated_test_execution_framework/AutomatedTestExecutionFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.automated_test_execution_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTestExecutionFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
